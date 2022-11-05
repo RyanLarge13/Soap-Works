675a81d330149926baaa9432afa2759463e4c50c
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-11-05 00:07:41 -0400

    Starting on the second section of the index page to render out a selection of kits available to purchase. In index.js for the index.ejs view, the dynamic code was created for creating dynamic heights on each individual soap product via the Math.random method and adding classes via classList. Styles were implimented for various elements and creating a solid consistent style and structure for the client to view.

--- views/js/index.js
+++ views/js/index.js
@@ -1,6 +1,21 @@
 const soapGrid = document.querySelector('.soaps');
-const masonry = new Masonry(soapGrid, {
-    itemSelector: '.grid-item',
-    columnWidth: 0,
-    percentPosition: true
+const soapItems = document.querySelectorAll('.grid-item');
+const getRandom = () => {
+    return Math.floor(Math.random() * 4);
+}
+soapItems.forEach((item) => {
+    let random = getRandom();
+    item.classList.add(`height-${random}`);
 });
+setTimeout(() => {
+    const masonry = new Masonry(soapGrid, {
+        itemSelector: '.grid-item',
+        columnWidth: 0,
+        percentPosition: true
+    }); 
+    for (let i = 0; i < soapItems.length; i++) {
+        setTimeout(() => {
+            soapItems[i].style.opacity = '1';
+        }, 250 * i);
+    }
+}, 500);
