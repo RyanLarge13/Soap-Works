5c8dd006e030954f2a65f9157a0039b3a6885854
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-11-15 14:49:17 -0800

    Simple update.

--- Frontend/partials/Cart/Cart.js
+++ Frontend/partials/Cart/Cart.js
@@ -4,25 +4,26 @@ export const showCart = (e) => {
   cart.classList.toggle("show");
   if (!cart.classList.contains("show")) return;
   getItems();
-  createElements();
 };
 
 const getItems = () => {
   const keys = Object.keys(localStorage);
   for (let i = 0; i < localStorage.length; i++) {
     const obj = JSON.parse(localStorage.getItem(keys[i]));
-    if (products.length === localStorage.length) continue;
-    else products.push(obj);
+    createElement(obj);
   }
 };
 
-const createElements = () => {
+const createElement = (obj) => {
   const cartContainer = document.querySelector(".cart-container");
-  products.forEach((item) => {
-    const itemDiv = document.createElement("div");
-    itemDiv.className = ".cart-item";
-    itemDiv.innerHTML = `<h1>${item.Title}</h1>`;
-    cartContainer.appendChild(itemDiv);
+  const existingItems = document.querySelectorAll(".cart-item");
+  if (obj.Title === undefined) return;
+  const itemDiv = document.createElement("div");
+  itemDiv.className = ".cart-item";
+  itemDiv.innerHTML = `<h1>${obj.Title}</h1>`;
+  existingItems.forEach((elem) => {
+    if (elem === itemDiv) return;
+  cartContainer.appendChild(itemDiv);
   });
 };
 
