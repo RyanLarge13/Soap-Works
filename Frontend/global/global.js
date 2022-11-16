4283a8dfd84cc44c336268bafb38670eedd5df5a
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-11-16 12:11:04 -0800

    Successfully fixing the bug for updating quantity via the cart and localstorage. Also implimenting a UI function for allowing users to easily close their shopping cart by clicking the backdrop element created.

--- Frontend/global/global.js
+++ Frontend/global/global.js
@@ -8,17 +8,26 @@ const buyBtns = document.querySelectorAll(".buy-btn");
 const setStorage = async (e) => {
   const parent = e.target.parentElement;
   const title = parent.querySelector("h2").innerText;
+  const cartShown = document.querySelector('.cart').classList.contains("show");
+  const itemExsists = localStorage.getItem(title);
+  const devUrl = 'http://localhost:8080';
+  const productionUrl = 'https://soap-works-production.up.railway.app';
+  let quantity = 1;
 
-  await fetch(`https://soap-works-production.up.railway.app/addtocart/${title}`, {
+  if (itemExsists) return;
+
+  await fetch(`${productionUrl}/addtocart/${title}`, {
     method: "GET",
   })
     .then((res) => res.json())
     .then((data) => {
       const formattedData = data.foundProduct[0];
       localStorage.setItem(`${title}`, JSON.stringify(formattedData));
+      localStorage.setItem(`${formattedData._id}`, `${quantity}`);
       createElement(formattedData);
     });
   showSoap();
+  if (cartShown) return;
   showCart();
 };
 
--- Frontend/partials/Cart/Cart.js
+++ Frontend/partials/Cart/Cart.js
@@ -1,20 +1,24 @@
-export const showCart = (e) => {
+export const showCart = () => {
   const cart = document.querySelector(".cart");
+  const backdrop = document.querySelector(".backdrop");
   cart.classList.toggle("show");
+  backdrop.classList.toggle("show");
+  backdrop.addEventListener('click', showCart);
 };
 
 export const getItems = () => {
-    const keys = Object.keys(localStorage);
-    for (let i = 0; i < localStorage.length; i++) {
-      const obj = JSON.parse(localStorage.getItem(keys[i]));
-      createElement(obj);
-    }
+  const keys = Object.keys(localStorage);
+  for (let i = 0; i < localStorage.length; i++) {
+    const obj = JSON.parse(localStorage.getItem(keys[i]));
+    createElement(obj);
+  }
 };
 
 export const createElement = (obj) => {
   const cartContainer = document.querySelector(".cart-container");
-  if (obj.Title === undefined) return;
   const itemDiv = document.createElement("div");
+  const quantity = localStorage.getItem(obj._id);
+  if (obj.Title === undefined) return;
   itemDiv.className = "cart-item";
   itemDiv.innerHTML = `<h1>${obj.Title}</h1>
   <div class="cart-img-container">
@@ -22,20 +26,56 @@ export const createElement = (obj) => {
   </div>
   <div class="quantity-container">
   <button class="dec">-</button>
-  <p class="quantity">1</p>
+  <p class="cart-item-quantity">${quantity}</p>
   <button class="inc">+</button>
   </div>
   <button class="danger">Delete</button>`;
   cartContainer.appendChild(itemDiv);
 
-  const deleteBtns = document.querySelectorAll('.danger');
+  //adding event listeners in the same function to avoid passing an unreasonable amnout of parameters.
+  const deleteBtns = document.querySelectorAll(".danger");
+  const incBtns = document.querySelectorAll(".inc");
+  const decBtns = document.querySelectorAll(".dec");
+  // const eachQuantity = document.querySelectorAll('.cart-item-quantity');
+  const checkoutBtn = document.querySelector(".checkout");
   deleteBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      cartContainer.removeChild(e.target.parentElement);
-      localStorage.removeItem(e.target.parentElement.firstElementChild.innerHTML);
+    btn.addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+      const htmlObj = e.target.parentElement;
+      cartContainer.removeChild(htmlObj);
+      localStorage.removeItem(obj.Title);
+      localStorage.removeItem(obj._id);
     });
   });
+  incBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+      const parent = e.target.parentElement;
+      let quantity = parent.querySelector(".cart-item-quantity");
+      let num = localStorage.getItem(obj._id);
+      num++;
+      localStorage.removeItem(obj._id);
+      localStorage.setItem(obj._id, `${num}`);
+      quantity.innerHTML = localStorage.getItem(obj._id);
+    });
+  });
+  decBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.stopImmediatePropagation();
+      const parent = e.target.parentElement;
+      let quantity = parent.querySelector(".cart-item-quantity");
+      let num = localStorage.getItem(obj._id);
+      if (num === "1") return;
+      num--;
+      localStorage.removeItem(obj._id);
+      localStorage.setItem(obj._id, `${num}`);
+      quantity.innerHTML = localStorage.getItem(obj._id);
+    });
+  });
+  checkoutBtn.addEventListener("click", checkout);
 };
+
+const checkout = () => {};
 
 document.querySelector("header").addEventListener("click", showCart);
 document.addEventListener("DOMContentLoaded", getItems);
