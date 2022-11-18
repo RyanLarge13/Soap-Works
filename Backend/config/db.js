a582b9566104282e6b9508c3218d3241c898cda8
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-11-18 14:28:46 -0800

    Updating mongodb connectiom to include new option for version greater than 3.1.0.

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(process.env.MONGODB_URI, {})
+    .connect(process.env.MONGODB_URI, {
+useNewUrlParser: true,
+   useUnifiedTopology: true
+    })
     .then((con) => console.log("You are connected to the database."))
     .catch((err) => console.log(err));
 };
