23ad873c0affa7bc2c1ea6d2102433e3655ce0a6
RyanLarge13 <ryanlarge13@gmail.com>
2023-01-29 21:59:07 -0800

    Updating basic styles

--- Frontend/pages/Index/js/showKits.js
+++ Frontend/pages/Index/js/showKits.js
@@ -4,9 +4,11 @@ const renderKit = () => {
 };
 
 const grabInfo = (e) => {
-  const type = 'kits';
-  const title = e.target.querySelector('h2').innerText.trim();
-  window.location.href = `http://localhost:8080/${type}/${title}`;
+  const productionUrl = "https://soap-works-production.up.railway.app";
+  const devUrl = "http://localhost:8080";
+  const type = "kits";
+  const title = e.target.querySelector("h2").innerText.trim();
+  window.location.href = `${productionUrl}/${type}/${title}`;
 };
 
 window.addEventListener("DOMContentLoaded", renderKit);
