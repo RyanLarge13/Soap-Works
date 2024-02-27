b49c2720d50086f706380a17e6524d18921a23b1
RyanLarge13 <ryanlarge13@gmail.com>
2024-02-26 21:34:08 -0500

    Updating Styles

--- Frontend/partials/Nav/Nav.js
+++ Frontend/partials/Nav/Nav.js
@@ -3,13 +3,28 @@ export const toggleMenu = (e) => {
   e.stopPropagation();
   const spans = document.querySelectorAll(".menu-toggle span");
   const nav = document.querySelector("nav");
+  const backdrop = document.querySelector(".nav-backdrop");
   nav.classList.toggle("open");
   spans.forEach((span) => {
     span.classList.toggle("x");
   });
+  backdrop.classList.toggle("show");
+};
+
+const closeMenu = (e) => {
+  e.stopPropagation();
+  const backdrop = e.target;
+  const nav = document.querySelector("nav");
+  const spans = document.querySelectorAll(".menu-toggle span");
+  nav.classList.remove("open");
+  spans.forEach((span) => {
+    span.classList.remove("x");
+  });
+  backdrop.classList.remove("show");
 };
 
 document.querySelector(".menu-toggle").addEventListener("click", toggleMenu);
+document.querySelector(".nav-backdrop").addEventListener("click", closeMenu);
 document.querySelectorAll("nav a").forEach((link) => {
   link.addEventListener("click", (e) => {
     e.stopImmediatePropagation();
